2995ecb962d8ec777d663f47d321912bb106cc36
Kacper Smietana <kabox9@gmail.com>
2024-03-18 08:30:38 +0100

    favicon added and prepared images in js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const galleryImg = document.querySelector(".gallery-img");
+
+const img1 = {
+    "path": "../img/furymale/m2top.jpg",
+    "name": ""
+}
+const img2 = {
+    "path": "../img/furymale/m4top.jpg",
+    "name": ""
+}
+const img3 = {
+    "path": "../img/furymale/a35top.jpg",
+    "name": ""
+}
+const img4 = {
+    "path": "../img/furymale/A7402270-min.jpg",
+    "name": ""
+}
+const img5 = {
+    "path": "../img/furymale/m2top.jpg",
+    "name": ""
+}
