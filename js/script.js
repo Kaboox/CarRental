710eda95fbb5c5493f8645989d81426bae6820d2
Kacper Smietana <kabox9@gmail.com>
2024-03-26 21:57:54 +0100

    dynamic gallery image alloction when loaded content

--- js/script.js
+++ js/script.js
@@ -1,28 +1,33 @@
 const galleryImg = document.querySelector(".gallery-img");
+const carName = document.querySelector(".car-name");
 const leftArrow = document.querySelector(".left");
 const rightArrow = document.querySelector(".right");
 const paymentAmount = document.querySelector('.payment-amount');
+const priceBtn = document.querySelector('.price-btn');
+const selectedCar = document.getElementById("car");
+const daysAmount = document.getElementById("days");
+const selectedKmAmount = document.getElementById("km");
 
 let imgCounter = 0;
 const img1 = {
-    "path": "../img/furymale/m2top.jpg",
-    "name": ""
+    "path": "../img/furymale/m2front.jpg",
+    "name": "BMW M2 Competition"
 }
 const img2 = {
-    "path": "../img/furymale/m4top.jpg",
-    "name": ""
+    "path": "../img/furymale/m2back.jpg",
+    "name": "BMW M2 Competition"
 }
 const img3 = {
-    "path": "../img/furymale/a35top.jpg",
-    "name": ""
+    "path": "../img/furymale/m4front.jpg",
+    "name": "BMW M4 Competition"
 }
 const img4 = {
-    "path": "../img/furymale/A7402270-min.jpg",
-    "name": ""
+    "path": "../img/furymale/m4back.jpg",
+    "name": "BMW M4 Competition"
 }
 const img5 = {
-    "path": "../img/furymale/m2top.jpg",
-    "name": ""
+    "path": "../img/furymale/a35front.jpg",
+    "name": "Mercedes A35 AMG"
 }
 const imgArray = [
     img1,
@@ -37,7 +42,8 @@ const moveLeft = (event) => {
     if (imgCounter < 0) {
         imgCounter = imgArray.length - 1;
     }
-    galleryImg.setAttribute('src', imgArray[imgCounter].path)
+    galleryImg.setAttribute('src', imgArray[imgCounter].path);
+    carName.textContent = imgArray[imgCounter].name;
 }
 
 const moveRight = (event) => {
@@ -46,7 +52,18 @@ const moveRight = (event) => {
         imgCounter = 0;
     }
     galleryImg.setAttribute('src', imgArray[imgCounter].path)
+    carName.textContent = imgArray[imgCounter].name;
+}
+
+const countPayment = (event) => {
+    event.preventDefault();
+
 }
 
 leftArrow.addEventListener('click', moveLeft);
 rightArrow.addEventListener('click', moveRight);
+priceBtn.addEventListener('click', countPayment);
+document.addEventListener('DOMContentLoaded', () => {
+    galleryImg.setAttribute('src', imgArray[imgCounter].path);
+    carName.textContent = imgArray[imgCounter].name;
+})
